eafe54ce066ae2dbbda85cd1218faec6dff7e2e5
LGabelashvili <gabelashvili1999@gmail.com>
2023-01-14 23:48:20 +0400

    init project

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,7 @@
+import './layout.scss';
+
+const Layout = () => {
+  return <div className="layout">Layout</div>;
+};
+
+export default Layout;
---
+++ src/components/root/App.js
@@ -0,0 +1,13 @@
+import Layout from '../Layout/Layout';
+
+import './app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
